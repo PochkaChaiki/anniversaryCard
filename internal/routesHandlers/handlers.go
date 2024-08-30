df1087de831160921b2f31267667bea9a7bc79da
PochkaChaiki <pochka.chaiki@gmail.com>
2024-08-30 17:59:41 +0300

    Added a few minor fixes. Changed route from client html

--- internal/routesHandlers/handlers.go
+++ internal/routesHandlers/handlers.go
@@ -11,50 +11,40 @@ import (
 
 func GetStaticHandler(address *string, port *string) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodGet {
-			templ, err := template.New("index.html").ParseFiles("./static/index.html")
-			if err != nil {
-				log.Print(err.Error())
-				http.Error(w, http.StatusText(500), http.StatusInternalServerError)
-				return
-			}
-			data := struct {
-				IPAddress string
-				Port      string
-			}{
-				IPAddress: *address,
-				Port:      *port,
-			}
-			err = templ.Execute(w, data)
-			if err != nil {
-				log.Print(err.Error())
-				http.Error(w, http.StatusText(500), http.StatusInternalServerError)
-				return
-			}
-		} else {
-			http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
+		templ, err := template.New("index.html").ParseFiles("./static/index.html")
+		if err != nil {
+			log.Print(err.Error())
+			http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+			return
+		}
+		data := struct {
+			IPAddress string
+			Port      string
+		}{
+			IPAddress: *address,
+			Port:      *port,
+		}
+		err = templ.Execute(w, data)
+		if err != nil {
+			log.Print(err.Error())
+			http.Error(w, http.StatusText(500), http.StatusInternalServerError)
 			return
 		}
 	}
 }
 
 func GetImageHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		cardText, err := dbmanager.GetString()
-		if err != nil {
-			log.Fatalf("error db getstring: %v\n", err)
-			return
-		}
-		img, err := imageprocessor.CreateImage(cardText)
-		if err != nil {
-			log.Fatalf("error create image: %v", err)
-			return
-		}
-		png.Encode(w, img)
-	} else {
-		http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
+	cardText, err := dbmanager.GetString()
+	if err != nil {
+		log.Fatalf("error db getstring: %v\n", err)
 		return
 	}
+	img, err := imageprocessor.CreateImage(cardText)
+	if err != nil {
+		log.Fatalf("error create image: %v", err)
+		return
+	}
+	png.Encode(w, img)
 }
 
 func EnableCORS(next http.Handler) http.Handler {
